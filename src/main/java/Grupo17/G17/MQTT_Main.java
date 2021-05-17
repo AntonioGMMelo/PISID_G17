621d08eeef9110394781453a7d76b939305ebfe9
mmfco1 <mmfco1@iscte-iul.pt>
2021-05-17 15:34:23 +0100

    mka

--- src/main/java/Grupo17/G17/MQTT_Main.java
+++ src/main/java/Grupo17/G17/MQTT_Main.java
@@ -6,11 +6,12 @@ import java.io.IOException;
 import java.util.Properties;
 
 import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+import com.sun.security.ntlm.Client;
 
 public class MQTT_Main {
 	
-	
-
 	public static void main(String[] args) throws InterruptedException, MqttException, FileNotFoundException, IOException {
 		
 		Properties p = new Properties();
@@ -21,19 +22,19 @@ public class MQTT_Main {
 		
 		MQTT_MongoDBPublisher cliente = new MQTT_MongoDBPublisher(cloudServerName);
 		
-		//cliente.iniciar();
 		cliente.connectCloud(p);
 		
 		new MQTT_MySQLSubscriber(cliente, cloudTopicName, 0);
 		
 		while(true) {
 			
-			Thread.sleep(1000);
-			
 		//	String mensagem = p.getProperty("Zona ," + "Sensor ," + "LimiteInferior ," + "LimiteSuperior ," + "ValorInicial");
+			//cliente.getMongo();
+			String mensagem = "";
 			
-			String mensagem = "exemplo";
 			cliente.publicar(cloudTopicName, mensagem.getBytes(), 0);
+			
+			Thread.sleep(1000);
 		}
 
 	}
--- src/main/java/Grupo17/G17/MQTT_MongoDBPublisher.java
+++ src/main/java/Grupo17/G17/MQTT_MongoDBPublisher.java
@@ -1,5 +1,6 @@
 package Grupo17.G17;
 
+import java.awt.List;
 import java.io.FileInputStream;
 import java.util.Arrays;
 import java.util.Properties;
@@ -20,6 +21,8 @@ import org.eclipse.paho.client.mqttv3.persist.MqttDefaultFilePersistence;
 import com.mongodb.DB;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
@@ -29,9 +32,10 @@ public class MQTT_MongoDBPublisher implements MqttCallbackExtended{
 	private final String serverURI;
 	private MqttClient client;
 	private final MqttConnectOptions mqttOptions;
-	private MongoClient mongoClient;
-	private MongoDatabase mongoDataBase;
-	private MongoCollection<Document> mongoCollection;
+	private boolean initial = true;
+	private Document[] medicoes;
+	com.mongodb.client.MongoClient mongo = MongoClients.create("mongodb://127.0.0.1:27017");
+	public MongoDatabase db = mongo.getDatabase("EstufaDB");
 	
 	
 	
@@ -65,7 +69,6 @@ public class MQTT_MongoDBPublisher implements MqttCallbackExtended{
 	            return null;
 	        }
 	    }
-	
 	 
 	 
 	 public void publicar(String topic, byte[] payload, int qos) throws MqttException {
@@ -82,8 +85,6 @@ public class MQTT_MongoDBPublisher implements MqttCallbackExtended{
 //		 
 //	 }
 	 
-	 
-	 
 	 public void connectCloud(Properties p) throws MqttException {
 		 String cloudServerName = p.getProperty("cloud_server");
 		 String cloudTopicName = p.getProperty("cloud_topic");
@@ -98,17 +99,52 @@ public class MQTT_MongoDBPublisher implements MqttCallbackExtended{
 		 }catch (MqttException e) {
 			e.printStackTrace();
 		}
-		 
-		 
-		 
-		 
+		  
 	 }
+	 
+//	public synchronized void getMensagem() {
+//		
+//		FindIterable<Document> mycursor1 = (FindIterable<Document>) db.getCollection("Zona1");
+//		FindIterable<Document> mycursor2 = (FindIterable<Document>) db.getCollection("Zona2");
+//		
+//		int a = 0;
+//		
+//		while(a<3) {
+//			Document[] aux = new Document[1000];
+//			Document obj = mycursor1.iterator().next();
+//			if(initial) {
+//				aux[0] = obj;
+//				initial = false;
+//			}
+//			else	
+//				for(int i = 0; i < aux.length; i++) {
+//					if(aux[i] == null) {
+//						System.out.println("devo");
+//						aux[i] = obj;
+//						i = aux.length;
+//					}
+//				}
+//			
+//			medicoes = aux;
+//			System.out.println(medicoes[0].toString());
+//			a++;
+//		}
+//		
+//		
+//		while(mycursor2.iterator().hasNext()) {
+//			System.out.println("chegou2");
+//			Document obj = mycursor2.iterator().next();
+//			medicoes[medicoes.length+1] = obj;
+//		}
+//		
+//		System.out.println(medicoes[1]);
+//			
+//	}
+	 
 
 	public synchronized void publicar(String topic, byte[] payload, int qos, boolean retained) throws MqttException {
-		 //client = new MqttClient("tcp://localhost:27017", MqttClient.generateClientId());
-		// client = new MqttClient("mongo://localhost:27017", MqttClient.generateClientId());
+		
 		 try {
-			// connectMongo();
 			 if(client.isConnected()) {
 				 client.publish(topic, payload, qos, retained);
 				 System.out.println("Topico publicado");
@@ -120,11 +156,6 @@ public class MQTT_MongoDBPublisher implements MqttCallbackExtended{
 		}
 	 }
 	 
-	//-----------------------------------------------------------------------------------------------
-	 //MongoDB
-	 
-
-	 
 
 	@Override
 	public void connectionLost(Throwable cause) {
@@ -134,6 +165,9 @@ public class MQTT_MongoDBPublisher implements MqttCallbackExtended{
 
 	@Override
 	public void messageArrived(String topic, MqttMessage message) throws Exception {
+		
+		
+		
 	}
 
 	@Override
