8aaed72aba5d8c9c87bc8fe15ea980ca4452082c
Miguel Neto <mrnos1@iscte-iul.pt>
2021-05-20 00:03:19 +0100

    Update

--- src/main/java/Grupo17/G17/Getter.java
+++ src/main/java/Grupo17/G17/Getter.java
@@ -141,7 +141,7 @@ public class Getter extends Thread{
 		return listaMedicoes;
 	}
 
-	public Boolean checkTimestamp(String timestamp, String ultimo_timestamp) {
+	public Boolean checkTimestamp(String timestamp, String ultimo_timestamp) {		
 		String[] t = timestamp.split(" at ");
 		String[] data = t[0].split(":");
 		String[] hora = t[1].split("/");
@@ -199,16 +199,7 @@ public class Getter extends Thread{
 				SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
 				java.util.Date utilDate = format.parse(m.toString().split(", ")[3].split("=")[1].split(" ")[0]);
 				java.sql.Date sqlDate = new java.sql.Date(utilDate.getTime());
-				if(finalTimestamp == null){
-					inserir = "INSERT INTO Medicao (Hora, Leitura, Valido, Zona_ID, Sensor_ID)" + "\r\n"
-							+ "VALUES (" + "'" + sqlDate + " " + m.toString().split(", ")[3].split("=")[1].split(" ")[2] + "'," 
-							+ "'" + Double.parseDouble(m.toString().split(", ")[4].split("=")[1].split("}")[0]) + "'" + "," +  "'1'" + "," 
-							+ "'" + m.toString().split(", ")[1].split("=")[1] + "'" + "," + "'" + m.toString().split(", ")[2].split("=")[1]  + "'" + ")";
-					System.out.println(inserir);
-					stm.executeUpdate(inserir);
-					finalTimestamp = sqlDate + " at " + m.toString().split(", ")[3].split("=")[1].split(" ")[2];
-				}
-				else if(!checkTimestamp(finalTimestamp, sqlDate + " at " + m.toString().split(", ")[3].split("=")[1].split(" ")[2])) {
+				if(checkTimestamp(sqlDate + " at " + m.toString().split(", ")[3].split("=")[1].split(" ")[2], finalTimestamp)) {
 					inserir = "INSERT INTO Medicao (Hora, Leitura, Valido, Zona_ID, Sensor_ID)" + "\r\n"
 							+ "VALUES (" + "'" + sqlDate + " " + m.toString().split(", ")[3].split("=")[1].split(" ")[2] + "'," 
 							+ "'" + Double.parseDouble(m.toString().split(", ")[4].split("=")[1].split("}")[0]) + "'" + "," +  "'1'" + "," 
@@ -224,10 +215,10 @@ public class Getter extends Thread{
 		}
 
 		conn.close();
+		
 		System.out.println("Last date sent: " + finalTimestamp);
 		File myObj = new File("timestamp.txt");
 		try (PrintWriter out = new PrintWriter("timestamp.txt")) {
-			System.out.println("chegou");
 			out.println(finalTimestamp);
 		}
 	}
