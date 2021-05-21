683aeb7a944a52d874637490c89edc59ddfbe47b
mmfco1 <mmfco1@iscte-iul.pt>
2021-05-21 15:09:53 +0100

    done

--- src/main/java/Grupo17/G17/MqttToMysql.java
+++ src/main/java/Grupo17/G17/MqttToMysql.java
@@ -3,6 +3,7 @@ package Grupo17.G17;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.util.Deque;
 import java.util.LinkedList;
 import java.util.Properties;
@@ -10,9 +11,11 @@ import java.util.Queue;
 import java.nio.charset.StandardCharsets;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
 import java.util.UUID;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
@@ -27,6 +30,7 @@ import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.eclipse.paho.client.mqttv3.MqttSecurityException;
 
+import com.google.protobuf.Timestamp;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoCollection;
@@ -43,11 +47,7 @@ public class MqttToMysql implements MqttCallback {
 	
 	public static void main(String[] args) throws InterruptedException, MqttSecurityException, MqttException, FileNotFoundException, IOException, SQLException {
 		
-		Connection conn = null;
-		Statement stm = null;
 		
-		conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/EstufaDB", "root", "");
-		stm = conn.createStatement();
 		
 		String cloudServer = "tcp://broker.mqtt-dashboard.com:1883";
 		cloudTopic = "g17";
@@ -85,36 +85,31 @@ public class MqttToMysql implements MqttCallback {
 	@Override
 	public void messageArrived(String arg0, MqttMessage arg1) throws Exception {
 		System.out.println(arg1.toString());
-		
-		Connection conn = null;
-		Statement stm = null;
-		String inserir=null;
-	
-
-		conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/EstufaDB", "root", "");
-		stm = conn.createStatement();
-		
-		
-//		MongoDatabase localMongoDatabase = localMongoClient.getDatabase("EstufaDB");
-//		 
-//	    MongoCollection<Document> localMongoCollection1 = localMongoDatabase.getCollection("Zona1");
-		
-		
-	    
-	    Document teste = new Document();
-	    
+		try {
+		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/EstufaDB", "root", "");
 	    String rawMsg = arg1.toString();
 	    
 	    String[] split = rawMsg.split(",");
-	   
-	    String helperZona = split[0].split(":")[1].trim();
-	    String helperSensor = split[1].split(":")[1].trim();
-	    String helperData = split[2].split(":", 2)[1].trim();
-	    String helperMedicao = split[3].split(":")[1].trim();
-	    double valid = 1;
-	    double medicao = Double.parseDouble(helperMedicao);
 	    
-	    if(Math.sqrt(Math.pow( medicao - medicoes.getLast(),2)) > 1) {
+	   
+	    String helperZona = split[1].split("=")[1].trim();
+	    
+	    
+	    String helperSensor = split[2].split("=")[1].trim();
+	    
+	    String helperData = split[3].split("=", 2)[1].replace("T", " ").replace("Z", "").trim();
+	    
+	    
+	   // Timestamp t = new Timestamp(DateUtil.provideDateFormat().parse(helperData).getTime());
+	    
+	    System.out.println(helperData);
+	    
+	    String helperMedicao = split[4].split("=")[1].trim();
+	    double valid = 1;
+	    double medicao = Double.parseDouble(helperMedicao.substring(0, 8));
+	    
+	    
+	    if(medicoes.size()>0 && Math.sqrt(Math.pow( medicao - medicoes.getLast(),2)) > 1) {
 	    	
 	    	valid = 0;
 	    	
@@ -146,9 +141,9 @@ public class MqttToMysql implements MqttCallback {
 	    
 	    
 	    
-	    SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
-	    java.util.Date utilDate = format.parse(rawMsg.split(", ")[3].split("=")[1].split(" ")[0]);
-	    java.sql.Date sqlDate = new java.sql.Date(utilDate.getTime());
+//	    SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+//	    java.util.Date utilDate = format.parse(rawMsg.split(", ")[3].split("=")[1].split(" ")[0]);
+//	    java.sql.Date sqlDate = new java.sql.Date(utilDate.getTime());
 	    
 //	    if(cliente.getMessage()!=null) {
 	   
@@ -158,13 +153,32 @@ public class MqttToMysql implements MqttCallback {
  
 	  //  MongoCursor<Document> cursor = localMongoCollection1.find().iterator();
 	    
-	    inserir = "INSERT INTO Medicao (Zona, Sensor, Data, Medicao)" +
-		        "VALUES (" + helperZona +", " + helperSensor + " ,"+ helperData + " ," + helperMedicao + " )";
-	    
-		System.out.println(inserir);
-		stm.executeUpdate(inserir);
-
+	    String addMedicao = "INSERT INTO Medicao (Medicao_ID, Hora, Leitura, Valido, Zona_ID, Sensor_ID)" +
+		        "VALUES ( ?, ?, ?, ?, ?, ?)";
 		
+		PreparedStatement preparedStatement = conn.prepareStatement(addMedicao);
+		preparedStatement.setString(1, null);
+		BigDecimal d = new BigDecimal(medicao);
+		preparedStatement.setString(2, helperData);
+		preparedStatement.setBigDecimal(3, d);
+		preparedStatement.setInt(4, (int) valid);
+		preparedStatement.setString(5, helperZona);
+		preparedStatement.setString(6, helperSensor);
+		preparedStatement.executeUpdate(); 
+		
+//	    PreparedStatement preparedStatement = conn.prepareStatement(addMedicao);
+//		preparedStatement.setString(1, null);
+//		BigDecimal d = new BigDecimal(100);
+//		preparedStatement.setString(2, "2021-05-21 13:53:15");
+//		preparedStatement.setBigDecimal(3, d);
+//		preparedStatement.setInt(4, 1);
+//		preparedStatement.setString(5, "Z1");
+//		preparedStatement.setString(6, "H1");
+//		preparedStatement.executeUpdate(); 
+		}catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("qualquer merda");
+		}
 	}
 
 }
